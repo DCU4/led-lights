830b62367cf967ce2dc67bf61f9f7b9ad1878a28
Dylan Pi <dylanjconnor4@gmail.com>
2024-04-17 02:35:17 +0100

    set up cookie for on checkbox status

--- public/main.js
+++ public/main.js
@@ -1,5 +1,5 @@
 const api_key = '27a2f910-e4ee-48f3-ad3c-81f389d939f1';
-let url = '192.168.1.186:8081';
+let url = '192.168.1.177:8081';
 // let url = 'localhost:8081';
 
 
@@ -13,11 +13,19 @@ window.addEventListener("load", function() { //when page loads
     const sexyTimeSwitch = document.getElementById("sexy-time");
     const colorChanger = document.querySelector('#color-changer');
 
+    const lightIsOn = getCookie("light-on");
+    if(lightIsOn == 'true') {
+      lightSwitch.checked = true;
+    }
+
     lightSwitch.addEventListener("change", function() { //add event listener for when checkbox changes
       socket.emit("light", Number(this.checked)); //send button status to server (as 1 or 0)
       if(!this.checked) {
         rainbowSwitch.checked = false;
         sexyTimeSwitch.checked = false;
+        setCookie("light-on", 'false', 365);
+      } else {
+        setCookie("light-on", 'true', 365);
       }
     });
 
@@ -38,6 +46,30 @@ window.addEventListener("load", function() { //when page loads
 });
 
 
+const setCookie = (cname, cvalue, exdays = 1) => {
+  var d = new Date();
+  d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+  var expires = "expires=" + d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+const getCookie = (cname) => {
+  var name = cname + "=";
+  var ca = document.cookie.split(';');
+  for (var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+
+
 function emitMessage(el, msg) {
   const emitter = document.getElementById(el);
   emitter.addEventListener("change", function() {
@@ -46,7 +78,7 @@ function emitMessage(el, msg) {
 }
 
 
-socket.on('light', function(data) { //get button status from client
+socket.on('light', function(data) { //get button status from server
   let status = document.querySelector('#light-status');
   var lightSwitch = document.getElementById("light");
   if (data == 1){
@@ -58,9 +90,6 @@ socket.on('light', function(data) { //get button status from client
   }
 });
 
-// socket.on('rainbow', function(data) { //get button status from client
-//   console.log(data)
-// });
 
 
 function colorChange(colorChanger) {
